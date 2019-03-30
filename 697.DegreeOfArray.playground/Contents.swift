ead3fceff7e00551352d7c132d216f0cd4fd08c0
Nipun <nipun@Admins-MacBook-Pro-3.local>
2019-03-29 22:51:40 -0700

    Degree Of Array

---
+++ 697.DegreeOfArray.playground/Contents.swift
@@ -0,0 +1,37 @@
+import UIKit
+
+class A {
+    var count:Int = 0
+    var startIndex:Int = -1
+    var length:Int = -1
+}
+
+func DegreeOfArray(myArray:[Int])->Int {
+    // Create dictionary to map value to count
+    var counts = [Int: A]()
+    
+    // Count the values with using forEach
+    var maxElementCount:Int = -1
+    for (index, element) in myArray.enumerated() {
+        if counts[element] == nil {
+            counts[element] = A()
+        }
+        counts[element]?.count = (counts[element]?.count)! + 1
+        if maxElementCount < (counts[element]?.count)! {
+            maxElementCount = element
+        }
+        
+        if counts[element]?.startIndex == -1 {
+            counts[element]?.startIndex = index
+        } else {
+            counts[element]?.length = index - (counts[element]?.startIndex)! + 1
+        }
+    }
+    
+    
+    return counts[maxElementCount]?.length ?? 0
+}
+
+print(DegreeOfArray(myArray: [1,2,2,3,1,4,2]))
+print(DegreeOfArray(myArray: [1, 2, 2, 3, 1]))
+
