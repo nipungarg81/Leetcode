e59489348cb8addc385b765d695d5ea4ba3beb09
Nipun <nipun@Admins-MacBook-Pro-3.local>
2019-04-02 22:18:33 -0700

    coding

--- CodingChallenge/ReverseTheWordInString.playground/Contents.swift
+++ CodingChallenge/ReverseTheWordInString.playground/Contents.swift
@@ -1,3 +1,21 @@
 import UIKit
 
-var str = "Hello, playground"
+let inputStr = "hello world? i love apple."
+let splitStr = inputStr.split{ [".", "?"].contains(String($0)) }
+
+var finalStr:String = ""
+for sStr in splitStr {
+    let splitSpaceStr = sStr.split(separator: " ")
+    let space = " "
+    for i in (0..<splitSpaceStr.count).reversed() {
+        finalStr += "\(splitSpaceStr[i])"
+        if i != 0 {
+            finalStr += space
+        }
+        
+    }
+    finalStr += ". "
+}
+
+print(finalStr)
+
--- CodingChallenge/SortValueBasedOnOccuranceArray.playground/Contents.swift
+++ CodingChallenge/SortValueBasedOnOccuranceArray.playground/Contents.swift
@@ -1,3 +1,21 @@
 import UIKit
 
-var str = "Hello, playground"
+let arr = [1, 2, 2, 2, 1, 3, 3, 4, 5, 5]
+var counts: [Int: Int] = [:]
+
+for item in arr {
+    counts[item] = (counts[item] ?? 0) + 1
+}
+
+let sortedByValueDictionary = counts.sorted {
+    if $0.1 == $1.1 {
+        return $0.0 < $1.0
+    } else {
+        return $0.1 > $1.1
+    }
+}
+
+let finalArray = sortedByValueDictionary.map {
+    return $0.key
+}
+print(finalArray)
